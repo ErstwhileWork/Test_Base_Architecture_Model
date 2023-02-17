b480b22398a3189269dd0b845a20ec6eecb0cb35
ErstwhileIIIWork <joseph@velocikey.com>
2023-02-16 19:14:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/basearchitecturemodel/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.basearchitecturemodel
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.basearchitecturemodel.ui.theme.BaseArchitectureModelTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BaseArchitectureModelTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting(getString(R.string.android_base_architecture_user_here))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BaseArchitectureModelTheme {
+        Greeting("Android")
+    }
+}
